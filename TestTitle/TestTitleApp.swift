ffaa4cde8e632059c40047006072435486d99ef7
Marta Korol <korolmarta490@gmail.com>
2025-02-28 01:25:02 +0200

    Initial project setup

---
+++ TestTitle/TestTitleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTitleApp.swift
+//  TestTitle
+//
+//  Created by Marta Korol on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTitleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
